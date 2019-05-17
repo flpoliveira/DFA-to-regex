30aabf36771ea8f193e18ff6951742653856f77a
RICARDO STOKLOSA <10129258970@edu.udesc.br>
2019-05-17 11:23:00 -0300

    Add function to get all transitions with a result

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import json
 
 def main():
     inputData = readInput('input.json')
-    print(inputData)
 
 
 def readInput(path):
     file = open(path, 'r')
     jsonData = json.loads(file.read())
     return jsonData
+
+
+def getAllTransitionsWithAListOfResults(listOfResults, transitions):
+    response = []
+    for x in transitions:
+        if(x['to'] in listOfResults):
+            response.append(x)
+    return response
 
 
 if __name__ == "__main__":
